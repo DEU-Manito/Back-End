808f4bbc17b3b22ddad76cd29a41a784528be4c5
donghyeon0915 <dongdong0915_@naver.com>
2022-05-02 02:05:49 +0900

    ADD : HTTPS(8443), HTTP(8080) 멀티 커넥트 설정, SSL 인증서 추가

--- src/main/java/deu/manito/DeuManitoApplication.java
+++ src/main/java/deu/manito/DeuManitoApplication.java
@@ -1,7 +1,11 @@
 package deu.manito;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeuManitoApplication {
@@ -10,4 +14,19 @@ public class DeuManitoApplication {
         SpringApplication.run(DeuManitoApplication.class, args);
     }
 
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory
+                = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createStandardConnector());
+
+        return tomcatServletWebServerFactory;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+
+        return connector;
+    }
 }
